c6eb73720020f7459134e8319a6e1c419c92786d
atimakov <atimakov@srv.rbt>
2021-12-04 23:14:24 +0300

    плавные переходы цветов + случайные радиусы

--- classes/Main.js
+++ classes/Main.js
@@ -1,13 +1,16 @@
 // класс определяющий параметры поля
 class Main {
     static init() {
-        Main.width = 600;
-        Main.height = 600;
+        Main.width = 1200;
+        Main.height = 800;
         Main.canvas = document.getElementById("main");
         Main.context = Main.create();
 
         Main.defaultColor = 'black';
         Main.context.fillStyle = Main.defaultColor;
+
+        Main.stepsDefault = 50;
+        Main.steps = Main.stepsDefault;
 
         Main.context.strokeRect(
             0,
@@ -17,18 +20,28 @@ class Main {
         );
 
         Main.color1 = [
-            [0,    256,  0],
-            [true, true, true]
+            [0, 256, 0],
+            [1, 1, 1]
         ];
 
         Main.color2 = [
-            [0,    0,  256],
-            [true, true, true]
+            [0, 0, 256],
+            [1, 1, 1]
         ];
 
         Main.color3 = [
-            [256,    0,  0],
-            [true, true, true]
+            [256, 0, 0],
+            [1, 1, 1]
+        ];
+
+        Main.color4 = [
+            [256, 256, 0],
+            [1, 1, 1]
+        ];
+
+        Main.radius = [
+            [50,80,120,200],
+            [1,1,1,1]
         ];
 
         Main.deg = 0;
@@ -40,12 +53,15 @@ class Main {
     static animate() {
         Main.context.clearRect(0, 0, Main.width, Main.height);
 
+        Main.chColorRandom(Main.radius);
+
         Main.draw4(Main.drawFr);
         Main.draw4(Main.drawFr3);
 
         Main.deg = Main.first++;
 
         Main.draw4(Main.drawFr2);
+        Main.draw4(Main.drawFr4);
 
         Main.deg = Main.second--;
 
@@ -68,37 +84,37 @@ class Main {
     }
 
     static drawFr() {
-        let i = 20;
-        let j = 20;
-        Main.chColor(Main.color3);
+        let i = 50;
+        let j = 50;
+        Main.chColorRandom(Main.color1);
         Main.drawCircle(
             i,
             j,
-            25,
+            Main.radius[0][0],
             "rgba(" +
-            Main.color3[0][0] + "," +
-            Main.color3[0][1] + "," +
-            Main.color3[0][2] +
+            Main.color1[0][0] + "," +
+            Main.color1[0][1] + "," +
+            Main.color1[0][2] +
             ",0.5)",
             true
         );
-        i = 40;
-        j = 40;
+        i = 100;
+        j = 100;
         Main.drawCircle(i, j, 7);
     }
 
     static drawFr2() {
         let i = 60;
         let j = 60;
-        Main.chColor(Main.color1);
+        Main.chColorRandom(Main.color2);
         Main.drawCircle(
             i,
             j,
-            80,
+            Main.radius[0][1],
             "rgba(" +
-                Main.color1[0][0] + "," +
-                Main.color1[0][1] + "," +
-                Main.color1[0][2] +
+                Main.color2[0][0] + "," +
+                Main.color2[0][1] + "," +
+                Main.color2[0][2] +
                 ",0.5)",
             true
         );
@@ -106,63 +122,65 @@ class Main {
         j = 80;
         Main.drawCircle(i, j, 11);
     }
-    static chColor(color){
-        if (color[0][0] > 256){
-            color[1][0] = false;
-        }
-        if (color[0][0] <= 0){
-            color[1][0] = true;
-        }
-        if (color[1][0]){
-            ++color[0][0];
-        }
-        else{
-            --color[0][0];
-        }
-        if (color[0][1] > 256){
-            color[1][1] = false;
-        }
-        if (color[0][1] <= 0){
-            color[1][1] = true;
-        }
-        if (color[1][1]){
-            ++color[0][1];
-        }
-        else{
-            --color[0][1];
-        }
-        if (color[0][2] > 256){
-            color[1][2] = false;
-        }
-        if (color[0][2] <= 0){
-            color[1][2] = true;
-        }
-        if (color[1][2]){
-            ++color[0][2];
-        }
-        else{
-            --color[0][2];
-        }
-    }
 
     static drawFr3() {
         let i = 100;
         let j = 100;
-        Main.chColor(Main.color2);
+        Main.chColorRandom(Main.color3);
         Main.drawCircle(
             i,
             j,
-            40,
+            Main.radius[0][2],
             "rgba(" +
-            Main.color2[0][0] + "," +
-            Main.color2[0][1] + "," +
-            Main.color2[0][2] +
+            Main.color3[0][0] + "," +
+            Main.color3[0][1] + "," +
+            Main.color3[0][2] +
             ",0.5)",
             true
         );
         i = 120;
         j = 120;
         Main.drawCircle(i, j, 5);
+    }
+
+    static drawFr4() {
+        let i = 160;
+        let j = 160;
+        Main.chColorRandom(Main.color4);
+        Main.drawCircle(
+            i,
+            j,
+            Main.radius[0][3],
+            "rgba(" +
+            Main.color4[0][0] + "," +
+            Main.color4[0][1] + "," +
+            Main.color4[0][2] +
+            ",0.5)",
+            true
+        );
+        i = 200;
+        j = 200;
+        Main.drawCircle(i, j, 5);
+    }
+
+    static chColorRandom(color){
+        for (let segmment in color[0]) {
+            if (color[0][segmment] >= 255) {
+                --color[0][segmment];
+            } else if (color[0][segmment] <= 0) {
+                ++color[0][segmment];
+            } else {
+                if (Main.steps-- <= 0) {
+                    if (Math.random() < 0.5) {
+                        color[1][segmment] = 1;
+                    } else {
+                        color[1][segmment] = -1;
+                    }
+                    Main.steps = Main.stepsDefault;
+                }
+                color[0][segmment] += color[1][segmment];
+            }
+        }
     }
 
     static drawCircle(i, j, radius, color = Main.defaultColor, fill = false) {
