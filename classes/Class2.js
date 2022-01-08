632114452ecdbe979694cfcf28192508ec4cd7b9
atimakov <atimakov@srv.rbt>
2022-01-08 18:39:15 +0300

    исправлена фигура, оптимизирован код

--- classes/Class2.js
+++ classes/Class2.js
@@ -3,7 +3,7 @@ class Class2 {
         Class2.defaultColor = 'black';
         Main.context.fillStyle = Class2.defaultColor;
 
-        Class2.step = 100;
+        Class2.step = 0;
 
         Class2.animate();
     }
@@ -12,51 +12,35 @@ class Class2 {
         Class2.drawFigure();
         ++Class2.step;
 
-        //requestAnimationFrame(Class2.animate);
+        requestAnimationFrame(Class2.animate);
     }
 
     static drawFigure(){
         const cnt = Main.context;
+        const coo = {
+            cp1x: Main.width / 2,
+            cp1y: Main.height / 2,
+            cp2x: Main.width / 2,
+            cp2y: Main.height / 2,
+            x: Main.width / 2,
+            y: Main.height / 2
+        };
+        const rad = Class2.step;
+        const delt = Class2.step * 2;
 
         cnt.clearRect(0, 0, Main.width, Main.height);
 
         cnt.beginPath();
-        ctx.moveTo(50,20);
-        cnt.bezierCurveTo(
-            Main.width / 2 + 50 + Class2.step,
-            Main.height / 2 + 50 + Class2.step,
-            Main.width / 2 + 50 + Class2.step,
-            Main.height / 2 + 50 + Class2.step,
-            Main.width / 2 + Class2.step,
-            Main.height / 2 + 50 + Class2.step);
-        cnt.bezierCurveTo(
-            Main.width / 2 - 50 - Class2.step,
-            Main.height / 2 + 50 + Class2.step,
-            Main.width / 2 - 50 - Class2.step,
-            Main.height / 2 + 50 + Class2.step,
-            Main.width / 2 - 50 - Class2.step,
-            Main.height / 2);
-        cnt.bezierCurveTo(
-            Main.width / 2 - 50 - Class2.step,
-            Main.height / 2 - 50 - Class2.step,
-            Main.width / 2 - 50 - Class2.step,
-            Main.height / 2 - 50 - Class2.step,
-            Main.width / 2,
-            Main.height / 2 - 50 - Class2.step);
-        cnt.bezierCurveTo(
-            Main.width / 2 + 50 + Class2.step,
-            Main.height / 2 - 50 - Class2.step,
-            Main.width / 2 + 50 + Class2.step,
-            Main.height / 2 - 50 - Class2.step,
-            Main.width / 2 + 50 + Class2.step,
-            Main.height / 2);
-        cnt.bezierCurveTo(
-            Main.width / 2 + 50 + Class2.step,
-            Main.height / 2 + 50 + Class2.step,
-            Main.width / 2 + 50 + Class2.step,
-            Main.height / 2 + 50 + Class2.step,
-            Main.width / 2 + Class2.step,
-            Main.height / 2 + 50 + Class2.step);
+        // право низ
+        cnt.moveTo(coo.x + rad,coo.y + rad);
+        // лево низ
+        cnt.bezierCurveTo(coo.cp1x,coo.cp1y + delt,coo.cp2x,coo.cp2y + delt,coo.x - rad,coo.y + rad);
+        // лево верх
+        cnt.bezierCurveTo(coo.cp1x - delt,coo.cp1y,coo.cp2x - delt,coo.cp2y,coo.x - rad,coo.y - rad);
+        // право верх
+        cnt.bezierCurveTo(coo.cp1x,coo.cp1y - delt,coo.cp2x,coo.cp2y - delt,coo.x + rad,coo.y - rad);
+        // право низ
+        cnt.bezierCurveTo(coo.cp1x + delt,coo.cp1y,coo.cp2x + delt,coo.cp2y,coo.x + rad,coo.y + rad);
         cnt.stroke();
     }
 }
